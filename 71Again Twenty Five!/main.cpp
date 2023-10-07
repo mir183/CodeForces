dbd05ab58594bbf6d744ddd882f7a5807385fc3b
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2023-10-08 02:58:00 +0600

    Add files via upload

---
+++ 71Again Twenty Five!/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    //int ans=pow(5,n);
+    //ans=to_string
+    cout<<25<<endl;
+}
---
+++ 71Again Twenty Five!/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int ans=pow(5,n);
+    cout<<ans<<endl;
+}
