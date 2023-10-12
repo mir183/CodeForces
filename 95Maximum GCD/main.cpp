b2cb03b779de1711579c068e95ed30dc849163a3
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2023-10-13 01:56:47 +0600

    Add files via upload

---
+++ 95Maximum GCD/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define fasterio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main() {
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        cout<<n/2<<endl;
+    }
+    
+    return 0;
+}
