2a7379fdd97cd426aee799f76a548ead177eb401
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2023-10-31 00:06:07 +0600

    Add files via upload

---
+++ 119Mahmoud and Ehab and the even-odd game/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define fasterio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main() {
+    fasterio;
+    int n;
+    cin>>n;
+    if(n%2==0)cout<<"Mahmoud"<<endl;
+    else cout<<"Ehab"<<endl;
+    return 0;
+}
