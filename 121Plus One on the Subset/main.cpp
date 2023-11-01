2ec6b7bc072124e3ced5cf9363af0f0a19b2c5e6
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2023-11-01 22:37:55 +0600

    Add files via upload

---
+++ 121Plus One on the Subset/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define fasterio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main() {
+    fasterio;
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)cin>>arr[i];
+        sort(arr,arr+n);
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+            ans=ans+abs(arr[i+1]-arr[i]);
+        }
+        cout<<ans<<endl;
+    }
+    
+    return 0;
+}
