67daf5eee058588570ceeb1561de05e3c666945b
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2023-12-08 09:32:14 +0600

    Add files via upload

---
+++ 146Halloumi Boxes/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define fasterio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main() {
+    fasterio;
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n,k;
+        cin>>n>>k;
+        vector<int>v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+        if(k>1){
+            yes;
+        }else{
+            vector<int> x=v;
+            sort(x.begin(),x.end());
+            if(x==v)yes;
+            else no;
+        }
+    }
+    
+    return 0;
+}
