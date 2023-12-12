0e5af990d827c86f6d34564d97ad16694efb509d
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2023-12-12 23:56:00 +0600

    Add files via upload

---
+++ 152Red and Blue Beans/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define fasterio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main() {
+    fasterio;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll r,b,d;
+        cin >> r >> b >> d;
+        if(r>b){
+            swap(r,b);
+        }
+        if(r*(d+1)>=b){
+            yes;
+        }
+        else no;
+    }
+    
+    return 0;
+}
