f0e36bc4cecd05baf2a91d1d2bb65c7192453d9e
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2023-12-17 10:08:10 +0600

    Add files via upload

---
+++ 157Constructive Problems/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define fasterio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main() {
+    fasterio;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        ll n,k;
+        cin >> n >> k;
+        cout<<max(n,k)<<endl;
+    }
+    
+    return 0;
+}
