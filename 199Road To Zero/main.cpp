e630e7661c1d26173a83257d7cdf9097263e8998
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-01-11 06:27:31 +0600

    Add files via upload

---
+++ 199Road To Zero/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define fasterio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll x,y,a,b;
+    cin >> x >> y >> a >> b;
+    b=min(b,2*a);
+    if(x<y) swap(x,y);
+    cout << (x-y)*a + y*b << endl;
+}
+
+int main() {
+    fasterio;
+    // Always use ll, int may give TLE
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
