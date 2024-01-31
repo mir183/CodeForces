b909b1a7057694144473e4488b3da664c6474cfe
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-01-31 21:17:43 +0600

    Add files via upload

---
+++ 227Brick Wall/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define fasterio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main() {
+    fasterio;
+    // Your code here
+    // Always use ll, int may give TLE
+    ll t;
+    cin>>t;
+    while (t--)
+    {
+        ll n,m;
+        cin>>n>>m;
+        ll ans=(m/2)*n;
+        cout<<ans<<endl;
+    }
+    
+    return 0;
+}
