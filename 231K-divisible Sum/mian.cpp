188a21140a441ac317cdfb9f36eaba45884c0437
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-02-02 23:17:51 +0600

    Add files via upload

---
+++ 231K-divisible Sum/mian.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define fasterio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main() {
+    fasterio;
+    // Your code here
+    // Always use ll, int may give TLE
+    ll t;
+    cin>>t;
+
+    while(t--){ll n,k;
+    cin>>n>>k;
+    ll x=(n+k-1)/k;
+    k*=x;
+    ll ans=(k+n-1)/n;
+    cout<<ans<<endl;}
+    return 0;
+}
