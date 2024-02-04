c83ded46ed69cec87ee515d70e53cc317e9df21f
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-02-04 23:55:56 +0600

    Add files via upload

---
+++ 233Nastia and Nearly Good Numbers/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define fasterio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+        ll a,b;
+    cin>>a>>b;
+    if(b==1){
+        no;
+        return;
+    }
+    
+        yes;
+        cout<<a<<" "<<a*b<<" "<<a+(a*b)<<endl;
+    
+}
+
+int main() {
+    fasterio;
+    // Always use ll, int may give TLE
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
