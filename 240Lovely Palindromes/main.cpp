704d9da37c04e7e9b2433ee0935385e13c027e59
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-02-11 23:41:24 +0600

    Add files via upload

---
+++ 240Lovely Palindromes/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define fasterio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main() {
+    fasterio;
+    // Your code here
+    // Always use ll, int may give TLE
+    string s;
+    getline(cin,s);
+    string ans=s;
+    reverse(s.begin(),s.end());
+    cout<<ans+s<<endl;
+
+    return 0;
+}
