caa88f756debb27d98d9d2208e7b81d45f9a1dd7
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-02-14 23:22:09 +0600

    Add files via upload

---
+++ 243Bad Ugly Numbers/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define fasterio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll n;
+    cin>>n;
+    if(n==1)cout<<-1<<endl;
+    else{
+        cout<<6;
+        for(ll i=0;i<n-1;i++){
+            cout<<9;
+        }
+        cout<<endl;
+    }
+}
+
+int main() {
+    fasterio;
+    // Always use ll, int may give TLE
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
