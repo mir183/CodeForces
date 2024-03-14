edc5e1684d9ad461005ca3a2781f252455801b5e
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-03-14 20:36:51 +0600

    Add files via upload

---
+++ 297K-th Not Divisible by n/m.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define fasterio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+int main() {
+    fasterio;
+    // Your code here
+    // Always use ll, int may give TLE
+    ll t;
+    cin>>t;
+    
+    while(t--){ll n,k;
+    cin>>n>>k;
+    ll x=(k-1)/(n-1);
+    cout<<k+x<<endl;}
+    return 0;
+}
