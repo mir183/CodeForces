887385966111dcc2ddbe794d65dfe1515e9f0d3e
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-04-09 01:17:52 +0600

    Add files via upload

---
+++ 326Yogurt Sale/m.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_PC ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll n,a,b;
+    cin>>n>>a>>b;
+    ll ans=0;
+    ans+=(n/2)*b;
+    if(n%2)ans+=a;
+    ans=min(ans,n*a);
+    cout<<ans<<endl;
+
+}
+
+signed main() {
+    MIR183_PC;
+    // Always use ll, int may give TLE
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
