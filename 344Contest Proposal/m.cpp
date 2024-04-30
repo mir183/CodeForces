04c17283dd09563853e749fd92812e9cf8293700
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-04-30 23:15:14 +0600

    Add files via upload

---
+++ 344Contest Proposal/m.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_PC ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll n;
+    cin>>n;
+    deque<ll>a(n),b(n);
+    ll ans=0;
+    for(ll i=0;i<n;i++)cin>>a[i];
+    for(ll i=0;i<n;i++)cin>>b[i];
+    for(ll i=0;i<n;i++){
+        if(a[i]>b[i]){
+            a.pop_back();
+            a.push_front(b[i]);
+            // a.resize();
+            ans++;
+        }
+    }
+    // cout<<"ANS: ";
+    cout<<ans<<endl;
+
+}
+
+signed main() {
+    MIR183_PC;
+    // Always use ll, int may give TLE
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
