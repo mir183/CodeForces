8dba3792fb321b0d29d89fd7997f22e4fd519f07
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-05-02 20:40:41 +0600

    Add files via upload

---
+++ 349Reverse Card (Easy Version)/m.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_PC ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll n,m;
+    cin>>n>>m;
+    ll ans=0;
+    for(ll i=1;i<=m;i++){
+        ll x=n/i+1;
+        ll y=x/i;
+        ans+=y;
+    }
+    ans--;
+    cout<<ans<<endl;
+}
+
+signed main() {
+    MIR183_PC;
+    // Always use ll, int may give TLE
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
