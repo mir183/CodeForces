78273a89bb7008cb50fa7ad99a5a13616e8d1c79
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-05-30 18:49:26 +0600

    Add files via upload

---
+++ 371Exciting Bets/m.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_PC ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll a,b;
+    cin>>a>>b;
+    if(a==b)cout<<0<<" "<<0<<endl;
+    else{
+        ll x=abs(a-b);
+        cout<<x<<" "<<min(a%x,x-a%x)<<endl;
+    }
+
+}
+
+signed main() {
+    MIR183_PC;
+    // Always use ll, int may give TLE
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
