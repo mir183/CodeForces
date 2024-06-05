dd928fab9573ba2a4fa918d7ff7e3e0489622fa9
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-06-05 22:48:12 +0600

    Add files via upload

---
+++ 376Distance and Axis/m.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_PC ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll n,k;
+    cin>>n>>k;
+    if(n<k){
+        cout<<k-n<<endl;
+    }
+    else if(n%2==k%2){
+        cout<<0<<endl;
+    }
+    else{
+        cout<<1<<endl;
+    }
+}
+
+signed main() {
+    MIR183_PC;
+    // Always use ll, int may give TLE
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
