fb15692464b930eae7e4087952ebb1cf9d3eb193
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-06-11 22:54:39 +0600

    Add files via upload

---
+++ 379Chips Moving/m.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_PC ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll n;
+    cin>>n;
+    ll cnt=0;
+    for(auto i=0;i<n;i++){
+        ll x;
+        cin>>x;
+        cnt+=x&1;
+
+    }
+    cout<<min(cnt,n-cnt)<<endl;
+}
+
+signed main() {
+    MIR183_PC;
+    // Always use ll, int may give TLE
+    ll t;
+    // cin >> t;
+    t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
