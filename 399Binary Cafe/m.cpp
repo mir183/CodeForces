216bc47991540089e250ac4cedf59fb7183ae60e
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-06-24 01:30:20 +0600

    Add files via upload

---
+++ 399Binary Cafe/m.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_PC ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll n,k;
+    cin>>n>>k;
+    ll x=log2(n);
+    ll ans=0;
+    if(x>=k){
+        ans=pow(2,k);
+    }else ans=n+1;
+    cout<<ans<<endl;
+}
+
+signed main() {
+    MIR183_PC;
+    // Always use ll, int may give TLE
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
