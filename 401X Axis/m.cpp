d4851987ebbb43d8beea48a4cc05df6bbf778c8c
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-06-24 23:45:31 +0600

    Add files via upload

---
+++ 401X Axis/m.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_PC ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll x,y,z;
+    cin>>x>>y>>z;
+    ll mx=max({x,y,z});
+    ll mn=min({x,y,z});
+    cout<<mx-mn<<endl;
+}
+
+signed main() {
+    MIR183_PC;
+    // Always use ll, int may give TLE
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
