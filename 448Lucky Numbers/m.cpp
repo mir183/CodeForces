cc09deec8e0799eff8c1636a97148e8ddcb79212
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-07-11 21:44:54 +0600

    Add files via upload

---
+++ 448Lucky Numbers/m.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<deque>
+#include<cmath>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_Mac ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll n;
+    cin>>n;
+    n++;
+    ll ans=pow(2,n);
+
+    // cout<<ans<<endl;
+    ans-=2;
+    cout<<ans<<endl;
+    
+
+
+}
+
+signed main() {
+    MIR183_Mac;
+    // Always use ll, int may give TLE
+    ll t;
+    // cin >> t;
+    t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
