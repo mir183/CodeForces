5e770e35ed38059dfe46abdb14772325879d0fc8
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-07-14 23:34:43 +0600

    Add files via upload

---
+++ 457Yet Another Meme Problem/m.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_Mac ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll a,b;
+    cin>>a>>b;
+    b++;
+    b=to_string(b).size();
+    ll ans=a*(b-1);
+    cout<<ans<<endl;
+}
+
+signed main() {
+    MIR183_Mac;
+    // Always use ll, int may give TLE
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
