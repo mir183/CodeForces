0a225cd5c92a9c39564ae28bb105bc24853bcb37
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-07-16 23:21:17 +0600

    Add files via upload

---
+++ 464Split the Multiset/m.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_Mac ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll n,k;
+    cin>>n>>k;
+    ll ans=0;
+    while (n>1)
+    {
+        ans++;
+        n-=min(n,k-1);
+    }
+    cout<<ans<<endl;
+}
+
+signed main() {
+    MIR183_Mac;
+    // Always use ll, int may give TLE
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
