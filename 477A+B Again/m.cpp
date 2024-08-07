d93cc5bab95d6b8f110db52927f4e066216a88a9
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-08-07 23:16:38 +0600

    Add files via upload

---
+++ 477A+B Again/m.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_Mac ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    string s;
+    cin>>s;
+    ll ans=0;
+    for(ll i=0;i<s.size();i++){
+        ans+=s[i]-48;
+    }
+    cout<<ans<<endl;
+}
+
+signed main() {
+    MIR183_Mac;
+    // Always use ll, int may give TLE
+    ll t;
+    cin >> t;
+    // t=1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
