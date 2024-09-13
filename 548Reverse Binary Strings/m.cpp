24e3781cdfcdfe833776b5badc1836e5d0c2abba
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-09-13 21:35:45 +0600

    Add files via upload

---
+++ 548Reverse Binary Strings/m.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_Mac ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    ll cnt=0;
+    for(ll i=1;i<n;i++){
+        if(s[i]==s[i-1])cnt++;
+    }
+    cout<<(cnt+1)/2<<endl;
+}
+
+signed main() {
+    MIR183_Mac;
+    // Always use ll, int may give TLE
+    ll tt;
+    // tt=1;
+    cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
