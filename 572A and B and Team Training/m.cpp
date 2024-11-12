5a1ede38f8cc83a1586f7196c8960d3c49984502
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-11-12 23:50:08 +0600

    Add files via upload

---
+++ 572A and B and Team Training/m.cpp
@@ -0,0 +1,34 @@
+//#include<bits/stdc++.h> //Uncomment while submitting
+#include <iostream>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_Mac ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl;
+
+void solve() {
+    // Your code here
+    ll a,b;
+    cin>>a>>b;
+    ll ans=min(a,b);
+    ll f=min(ans,(a+b)/3);
+    cout<<f<<endl;
+
+}
+
+signed main() {
+    MIR183_Mac;
+    // Always use ll, int may give TLE
+    ll tt;
+    tt=1;
+    // cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
