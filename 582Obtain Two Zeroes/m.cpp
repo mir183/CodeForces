c77d34587585e9bec2cdef1735d24268dc7cab30
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-11-19 22:54:29 +0600

    Add files via upload

---
+++ 582Obtain Two Zeroes/m.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h> //Uncomment while submitting
+#include <iostream>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_Mac ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl;
+
+void solve() {
+    // Your code here
+    ll x,y;
+    cin>>x>>y;
+    ll a=min(x,y);
+    ll b=max(x,y);
+    (a+b)%3==0 and 2*a>=b?yes:no;
+}
+
+signed main() {
+    MIR183_Mac;
+    // Always use ll, int may give TLE
+    ll tt;
+    // tt=1;
+    cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
