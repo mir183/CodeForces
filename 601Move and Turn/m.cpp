1930416913f86557289e3fff530ebd5744e521b1
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2024-12-03 22:37:33 +0600

    Add files via upload

---
+++ 601Move and Turn/m.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_PC ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll n;
+    cin>>n;
+    if(n%2){
+        cout<<2*(n/2+2)*(n/2+1)<<endl;
+    }else{
+        cout<<(n/2+1)*(n/2+1)<<endl;
+    }
+}
+
+signed main() {
+    MIR183_PC;
+    // Always use ll, int may give TLE
+    ll tt;
+    tt=1;
+    // cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
