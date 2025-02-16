152825946eacfd1f16c36361760f51e7e9f5473f
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2025-02-16 22:59:44 +0600

    Add files via upload

---
+++ 652Candy Box (easy version)/m.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h> //Uncomment while submitting
+#include <iostream>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_Mac ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl;
+
+void solve() {
+    // Your code here
+    ll n;
+    cin>>n;
+    deque<ll>v(n+1,0);
+    for(ll i=1;i<=n;i++){
+        ll x;
+        cin>>x;
+        ++v[x];
+    }
+    ll ans=0, k=LLONG_MAX;
+    sort(v.begin(),v.end());
+    for(ll i=n;i>=1;i--){
+        k--;
+        k=min(k,v[i]);
+        ans+=k;
+        if(k==0) break;
+    }
+    cout<<ans<<endl;
+}
+
+signed main() {
+    MIR183_Mac;
+    // Always use ll, int may give TLE
+    ll tt;
+    tt=1;
+    cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
