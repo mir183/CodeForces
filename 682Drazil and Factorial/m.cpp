4360d712cf024e11f50a1b0fa5bb9528741b855f
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2025-03-29 07:35:04 +0600

    Add files via upload

---
+++ 682Drazil and Factorial/m.cpp
@@ -0,0 +1,40 @@
+//#include<bits/stdc++.h> //Uncomment while submitting
+#include <iostream>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_Mac ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl;
+
+void solve() {
+    // Your code here
+    ll n;
+    cin>>n;
+    string s;
+    cin>>s;
+    deque<string>f={"", "", "2", "3", "322", "5", "53", "7", "7222", "7332"};
+    string ans="";
+    for(char c:s){
+        ans+=f[c-'0'];
+    }
+    sort(ans.rbegin(),ans.rend());
+    cout<<ans<<endl;
+
+}
+
+signed main() {
+    MIR183_Mac;
+    // Always use ll, int may give TLE
+    ll tt;
+    tt=1;
+    // cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
