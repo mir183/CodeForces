e9d414012dffd59fac597b76ec744a3ff38cf3cd
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2025-04-13 07:49:25 +0600

    Add files via upload

---
+++ 695Game With Array/m.cpp
@@ -0,0 +1,40 @@
+//#include<bits/stdc++.h> //Uncomment while submitting
+#include <iostream>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_Mac ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl;
+
+void solve() {
+    // Your code here
+    ll n,s;
+    cin>>n>>s;
+    if(n>s-n){
+        no;
+    }else{
+        yes;
+        for(ll i=0;i<n-1;i++){
+            cout<<2<<" ";
+            s-=2;
+        }
+        cout<<s<<endl<<1;
+    }
+}
+
+signed main() {
+    MIR183_Mac;
+    // Always use ll, int may give TLE
+    ll tt;
+    tt=1;
+    // cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
