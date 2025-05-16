943b76c73e5b5860f0b2d68892f1c8dc4a39ff06
Mir Hossain Ahmed <ahmedemon183@gmail.com>
2025-05-16 22:46:21 +0600

    Add files via upload

---
+++ 711Arithmetic Array/m.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define ld long double
+#define endl '\n'
+#define MIR183_PC ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+void solve() {
+    // Your code here
+    ll n;
+    cin>>n;
+    deque<ll>v(n);
+    for(ll i=0;i<n;i++){
+        cin>>v[i];
+    }
+    ll sum=accumulate(v.begin(),v.end(),0LL);
+    if(sum<n)cout<<1<<endl;
+    else cout<<sum-n<<endl;
+}
+
+signed main() {
+    MIR183_PC;
+    // Always use ll, int may give TLE
+    ll tt;
+    tt=1;
+    cin >> tt;
+    while (tt--) {
+        solve();
+    }
+    return 0;
+}
